f6fadd5e39f722dcf0fccb50e948c852a2986853
Zirui.Zhou <zirui.zhou19@student.xjtlu.edu.cn>
2022-03-15 13:13:18 +0800

    Initial commit

---
+++ xjtluapp-java/src/test/java/com/groupa15/XjtluappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupa15;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XjtluappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
