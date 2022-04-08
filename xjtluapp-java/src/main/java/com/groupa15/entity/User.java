b9363dde3d640a5bf5be111e4bd99e4bc9b01cb0
Zirui.Zhou <zirui.zhou19@student.xjtlu.edu.cn>
2022-04-08 13:26:35 +0800

    Mybatis-plus Integration (add user entity)

---
+++ xjtluapp-java/src/main/java/com/groupa15/entity/User.java
@@ -0,0 +1,23 @@
+package com.groupa15.entity;
+
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author Qi Xu
+ * @date 2022/3/15
+ */
+
+@Data
+@Accessors(chain = true)
+public class User {
+    @TableId
+    private Long userId;
+    private String userName;
+    private String password;
+    private String salt;
+    private LocalDateTime createTime;
+}
