15fb7addcf1bd4855b99b3bb32be0dac93479a4b
enosouces <1799131654@qq.com>
2022-04-09 17:53:14 +0800

    add secondary page by component for New Student,IT...

---
+++ xjtluapp-vue/src/components/main_page/conmponents/MultipleColumnCardList_sublist.vue
@@ -0,0 +1,91 @@
+<template>
+  <el-table :data="tableData" style="width: 100%">
+    <el-table-column type="expand">
+      <template #default="props">
+        <p>State: {{ props.row.state }}</p>
+        <p>City: {{ props.row.city }}</p>
+        <p>Address: {{ props.row.address }}</p>
+        <el-button type="primary" @click="openurl('https://guide.xjtlu.edu.cn/')">
+          <el-icon style="vertical-align: middle;">
+            <Key/>
+          </el-icon>
+          <span style="vertical-align: middle;"> 跳转 </span>
+        </el-button>
+
+
+
+      </template>
+    </el-table-column>
+    <el-table-column label="日期" prop="date"/>
+    <el-table-column label="标题" prop="name"/>
+  </el-table>
+
+</template>
+
+<script setup>
+import {Key} from '@element-plus/icons-vue';
+
+
+const openurl = (url) => {
+  window.open(url)
+}
+
+const tableData = [
+  {
+    date: '2016-05-03',
+    name: '新生入学',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+  },
+  {
+    date: '2016-05-02',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+  },
+  {
+    date: '2016-05-04',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+  },
+  {
+    date: '2016-05-01',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+  },
+  {
+    date: '2016-05-08',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+  },
+  {
+    date: '2016-05-06',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+  },
+  {
+    date: '2016-05-07',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+  },
+]
+</script>
