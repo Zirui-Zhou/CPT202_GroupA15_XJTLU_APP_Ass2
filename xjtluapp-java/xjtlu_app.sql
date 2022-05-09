06992c3afeed498c6ad682d0b997dc7458a94c76
Zirui.Zhou <zirui.zhou19@student.xjtlu.edu.cn>
2022-05-09 19:33:04 +0800

    Java part (add database sql file)

---
+++ xjtluapp-java/xjtlu_app.sql
@@ -0,0 +1,235 @@
+-- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: xjtlu_app
+-- ------------------------------------------------------
+-- Server version	8.0.28
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `article`
+--
+
+DROP TABLE IF EXISTS `article`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `article` (
+  `id` bigint NOT NULL,
+  `title` text CHARACTER SET utf8 COLLATE utf8_general_ci,
+  `content` text CHARACTER SET utf8 COLLATE utf8_general_ci,
+  `edit_time` datetime DEFAULT NULL,
+  `create_time` datetime DEFAULT CURRENT_TIMESTAMP,
+  `editor_id` bigint NOT NULL,
+  `image` text CHARACTER SET utf8 COLLATE utf8_general_ci,
+  PRIMARY KEY (`id`) USING BTREE,
+  KEY `user_id___fk` (`editor_id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `article_favourite`
+--
+
+DROP TABLE IF EXISTS `article_favourite`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `article_favourite` (
+  `article_id` bigint NOT NULL,
+  `user_id` bigint NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `article_history`
+--
+
+DROP TABLE IF EXISTS `article_history`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `article_history` (
+  `article_id` bigint NOT NULL,
+  `user_id` bigint NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `article_type`
+--
+
+DROP TABLE IF EXISTS `article_type`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `article_type` (
+  `type_id` int NOT NULL AUTO_INCREMENT,
+  `type_name` json NOT NULL,
+  `type_icon` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  PRIMARY KEY (`type_id`) USING BTREE,
+  UNIQUE KEY `article_type_type_id_uindex` (`type_id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `article_type_list`
+--
+
+DROP TABLE IF EXISTS `article_type_list`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `article_type_list` (
+  `article_id` bigint NOT NULL,
+  `type_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `guide`
+--
+
+DROP TABLE IF EXISTS `guide`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `guide` (
+  `guide_id` bigint NOT NULL,
+  `guide_title` text CHARACTER SET utf8 COLLATE utf8_general_ci,
+  `guide_date` date DEFAULT NULL,
+  `guide_link` text CHARACTER SET utf8 COLLATE utf8_general_ci,
+  `guide_type` int DEFAULT NULL,
+  PRIMARY KEY (`guide_id`) USING BTREE,
+  UNIQUE KEY `guide_guide_id_uindex` (`guide_id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `guide_type`
+--
+
+DROP TABLE IF EXISTS `guide_type`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `guide_type` (
+  `type_id` int NOT NULL AUTO_INCREMENT,
+  `type_name` json DEFAULT NULL,
+  `type_image` text CHARACTER SET utf8 COLLATE utf8_general_ci,
+  PRIMARY KEY (`type_id`) USING BTREE,
+  UNIQUE KEY `guide_type_type_id_uindex` (`type_id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `resource`
+--
+
+DROP TABLE IF EXISTS `resource`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `resource` (
+  `resource_id` bigint NOT NULL,
+  `resource_name` text CHARACTER SET utf8 COLLATE utf8_general_ci,
+  `resource_image` text CHARACTER SET utf8 COLLATE utf8_general_ci,
+  `resource_path` text CHARACTER SET utf8 COLLATE utf8_general_ci,
+  `create_time` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`resource_id`) USING BTREE,
+  UNIQUE KEY `resource_resource_id_uindex` (`resource_id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `resource_tag`
+--
+
+DROP TABLE IF EXISTS `resource_tag`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `resource_tag` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` json DEFAULT NULL,
+  `type` int NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE KEY `tag_id_uindex` (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `resource_tag_list`
+--
+
+DROP TABLE IF EXISTS `resource_tag_list`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `resource_tag_list` (
+  `resource_id` bigint NOT NULL,
+  `tag_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `resource_tag_type`
+--
+
+DROP TABLE IF EXISTS `resource_tag_type`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `resource_tag_type` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` json NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE KEY `tag_type_id_uindex` (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `user_id` bigint NOT NULL,
+  `user_name` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `password` text CHARACTER SET utf8 COLLATE utf8_general_ci,
+  `create_time` datetime DEFAULT CURRENT_TIMESTAMP,
+  `salt` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  PRIMARY KEY (`user_id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `user_info`
+--
+
+DROP TABLE IF EXISTS `user_info`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_info` (
+  `user_id` bigint NOT NULL,
+  `real_name` text CHARACTER SET utf8 COLLATE utf8_general_ci,
+  `major` text CHARACTER SET utf8 COLLATE utf8_general_ci,
+  `semester` text CHARACTER SET utf8 COLLATE utf8_general_ci,
+  `real_id` bigint DEFAULT NULL,
+  `email` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `avatar` text CHARACTER SET utf8 COLLATE utf8_general_ci,
+  PRIMARY KEY (`user_id`) USING BTREE,
+  UNIQUE KEY `user_info_user_id_uindex` (`user_id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-05-09 19:31:22
